3fe2a03e5cd5fcc578641697a4742045fde2c48e
Xu Lvcai <xulvcai2008@gmail.com>
2015-09-13 17:15:00 +0800

    tabs based implementation

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('current.controllers', ['ionic', 'current.services', 'current.directives'])
+
+.controller('CurrentCtrl', function($scope) {
+  
+})
+
+
+.controller('TrendCtrl', function($scope, Chats) {
+
+})
+
+
+.controller('SettingsCtrl', function($scope) {
+
+});
---
+++ www/js/directives.js
@@ -0,0 +1,37 @@
+angular.module('current.directives', [])
+
+
+.directive('currentDetail', function($timeout, $filter) {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'templates/current-detail.html',
+    scope: true,
+    link: function($scope, $element, $attr) {
+    }
+  }
+ })
+
+.directive('currentTime', function($timeout, $filter) {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'templates/current-time.html',
+    scope: true,
+    link: function($scope, $element, $attr) {
+    }
+  }
+ })
+
+.directive('currentWeather', function($timeout, $filter) {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: 'templates/current-weather.html',
+    scope: true,
+    link: function($scope, $element, $attr) {
+    }
+  }
+ })
+
+;
